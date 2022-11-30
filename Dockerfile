58d61230b2543f1e6388b921724bb001c46ee82d
l4blee <l4blee@yandex.ru>
2022-11-30 16:38:31 +0700

    upd

--- Dockerfile
+++ Dockerfile
@@ -15,6 +15,7 @@ WORKDIR /book_drook/
 COPY requirments.txt /book_drook/
 RUN pip install -r requirements.txt
 
+COPY ${SSL_PATH} /book_drook/certificates
 COPY backend/ /book_drook/backend
 COPY frontend/dist/ /book_drook/frontend/dist
 
--- backend/asgi.py
+++ backend/asgi.py
@@ -3,9 +3,8 @@ import ssl
 
 from app import app
 
-ssl_path = os.getenv('SSL_PATH')
 ctx = ssl.create_default_context(ssl.Purpose.CLIENT_AUTH)
-ctx.load_cert_chain(ssl_path + 'fullchain.pem', ssl_path + 'privkey.pem')
+ctx.load_cert_chain('certificates/fullchain.pem', 'certificates/privkey.pem')
 
 if __name__ == '__main__':
     app.run('0.0.0.0', int(os.getenv('PORT')), workers=1, ssl=ctx)
