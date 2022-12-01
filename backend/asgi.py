27490f79c9387eaa4459ee152d9d6cd02ce9a35c
l4blee <l4blee@yandex.ru>
2022-12-01 22:08:43 +0700

    upd

--- backend/asgi.py
+++ backend/asgi.py
@@ -1,10 +1,10 @@
 import os
-import ssl
+# import ssl
 
 from app import app
 
-ctx = ssl.create_default_context(ssl.Purpose.CLIENT_AUTH)
-ctx.load_cert_chain('certificates/fullchain.pem', 'certificates/privkey.pem')
+# ctx = ssl.create_default_context(ssl.Purpose.CLIENT_AUTH)
+# ctx.load_cert_chain('certificates/fullchain.pem', 'certificates/privkey.pem')
 
 if __name__ == '__main__':
-    app.run('0.0.0.0', int(os.getenv('PORT')), workers=1, ssl=ctx)
+    app.run('0.0.0.0', int(os.getenv('PORT')), workers=1) # , ssl=ctx)
