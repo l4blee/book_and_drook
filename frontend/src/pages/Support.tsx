b5d311f6936b0f1ff43f076b6a3b205919d2080d
l4blee <l4blee@yandex.ru>
2022-12-05 06:48:38 +0700

    upd

--- frontend/src/pages/Support.tsx
+++ frontend/src/pages/Support.tsx
@@ -12,7 +12,7 @@ const Support: Component = () => {
             m: '10px'
         }}>
            <Typography fontSize='1.5vw'>Реквизиты для поддержки библиотеки: </Typography> 
-           <Typography fontSize='2vw'>5469440013290060; Чирва Михаил Сергеевич</Typography> 
+           <Typography fontSize='2vw'>9137453927 - перевод на Сбербанк</Typography> 
            <Typography fontSize='2vw' fontWeight='bold' mt='5vw'>
                 Укажите в комментариях к переводу <span style='color: red'>библиотеку</span>, которую вы хотите поддержать, 
                 чтобы мы могли купить для неё книги
