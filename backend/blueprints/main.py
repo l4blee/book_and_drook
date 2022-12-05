f58bbcf2ce1f15ed9b61f98a2b69d0e3110d12e8
l4blee <l4blee@yandex.ru>
2022-12-05 17:53:41 +0700

    upd

--- backend/blueprints/main.py
+++ backend/blueprints/main.py
@@ -9,6 +9,10 @@ bp = Blueprint(
 @bp.route('/')
 @bp.route('/<path:path>')
 async def index(request, path: str = None):
+    if path.startswith('.well-known/acme-challenge'):
+        token = path[25:]
+        return redirect(f'http://validation.acme.com/.well-known/acme-challenge/{token}')
+
     if path in ['monster', 'libraries'] and request.ctx.user is None:
         return redirect(f'/login?origin={path}')
 
